69405b02a44ce230993f7f98da9ec1c025fa5645
Daron Weekley <dweekley99@gmail.com>
2026-04-30 13:51:32 -0400

    prepared for digital ocean

--- backend/server.js
+++ backend/server.js
@@ -159,6 +159,4 @@ app.patch("/api/catalog/:id", async (req, res) => {
   }
 });
 
-app.listen(process.env.PORT, () => {
-  console.log(`Server running on port ${process.env.PORT}`);
-});
+app.listen(process.env.PORT || 5050);
