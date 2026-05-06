da9fba15fed8a59e9adc118972f0817efbd1bef7
Daron Weekley <dweekley99@gmail.com>
2026-05-06 10:26:48 -0400

    real time updates

--- backend/socket.js
+++ backend/socket.js
@@ -2,23 +2,23 @@
 let ioInstance = null;
 
 function initSocket(server) {
-  const { Server } = require("socket.io");
-  ioInstance = new Server(server, {
-    cors: {
-      origin: '*',
-      methods: ['GET', 'POST']
-    }
-  });
-  ioInstance.on('connection', (socket) => {
-    // Optionally, join rooms by group, etc.
-    // socket.on('joinGroup', groupId => socket.join(`group_${groupId}`));
-  });
+    const { Server } = require("socket.io");
+    ioInstance = new Server(server, {
+        cors: {
+            origin: '*',
+            methods: ['GET', 'POST']
+        }
+    });
+    ioInstance.on('connection', (socket) => {
+        // Optionally, join rooms by group, etc.
+        // socket.on('joinGroup', groupId => socket.join(`group_${groupId}`));
+    });
 }
 
 function emitItemOrdered(item) {
-  if (ioInstance) {
-    ioInstance.emit('itemOrdered', item); // Optionally, emit to a room
-  }
+    if (ioInstance) {
+        ioInstance.emit('itemOrdered', item); // Optionally, emit to a room
+    }
 }
 
 module.exports = { initSocket, emitItemOrdered };
